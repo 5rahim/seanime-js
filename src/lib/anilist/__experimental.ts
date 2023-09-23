8f152aba87427d106dd6b66e85db5f5d73520d84
5rahim <bonfohzaki@gmail.com>
2023-09-22 22:16:16 -0400

    random

--- src/lib/anilist/__experimental.ts
+++ src/lib/anilist/__experimental.ts
@@ -6,46 +6,19 @@ import { logger } from "@/lib/helpers/debug"
 import rakun from "@/lib/rakun"
 import _ from "lodash"
 
-async function PromiseBatch(task: any, items: any, batchSize: number) {
-    let position = 0
-    let results: any[] = []
-    while (position < items.length) {
-        const itemsForBatch = items.slice(position, position + batchSize)
-        results = [...results, ...await Promise.all(itemsForBatch.map((item: any) => task(item)))]
-        position += batchSize
-    }
-    return results
-}
-
-// resolve anime name based on file name and store it
-const postfix: { [key: number]: string } = {
-    1: "st",
-    2: "nd",
-    3: "rd",
-}
-
-export async function getAllFileNames(
+export async function getMediaTitlesFromLocalDirectory(
     directoryPath: string,
-    stop: boolean = false,
 ) {
     try {
         let fileNames = new Set<string>()
         let titles = new Set<string>()
         const items: Dirent[] = await fs.readdir(directoryPath, { withFileTypes: true })
 
-        logger("repository/getAllFileNames").info("Getting all file fileNames")
+        logger("repository/getAllMediaTitles").info("Getting all file fileNames")
         for (const item of items) {
             if (item.name.match(/^(.*\.mkv|.*\.mp4|[^.]+)$/)) {
 
                 fileNames.add(item.name.replace(/(.mkv|.mp4)/, ""))
-                // const itemPath = path.join(directoryPath, item.name)
-                // const stats = await fs.stat(itemPath)
-                // if(stats.isDirectory() && !stop) {
-                //     const res = await getAllFileNames(itemPath, true)
-                //     res?.fileNames?.map(n => {
-                //         titles.add(rakun.parse(n)?.name?.trim() ?? undefined)
-                //     })
-                // }
 
             }
         }
@@ -58,7 +31,19 @@ export async function getAllFileNames(
             // titles: [].reduceRight((prev, curr) => prev.some(n => n.toLowerCase().includes(curr)) ? prev.filter(n => n !== prev.find(n => n.toLowerCase().includes(curr))) : prev, _.orderBy([...titles], [n => n, n => n.length], ["asc", "asc"]))
         }
     } catch (e) {
-        logger("repository/getAllFileNames").error("Failed")
+        logger("repository/getAllMediaTitles").error("Failed")
         return undefined
     }
+}
+
+
+async function PromiseBatch(task: any, items: any, batchSize: number) {
+    let position = 0
+    let results: any[] = []
+    while (position < items.length) {
+        const itemsForBatch = items.slice(position, position + batchSize)
+        results = [...results, ...await Promise.all(itemsForBatch.map((item: any) => task(item)))]
+        position += batchSize
+    }
+    return results
 }
